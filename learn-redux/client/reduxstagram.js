ddbcb52e35fd4b0b82e01ebed5ab3267b2285734
Tyler Mabey <mabeytyler@gmail.com>
2018-03-16 12:59:10 -0600

    add components, store

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -3,6 +3,24 @@ import React from 'react';
 
 import {render} from 'react-dom';
 
+// css
 import css from './styles/style.styl';
 
-render(<p>Hi</p>, document.getElementById('root'));
+// components
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+// react router
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+)
+
+render(router, document.getElementById('root'));
